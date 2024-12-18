525fa2fb8ca95d9aa30444fd7b767c5852138003
fgawry <f.etcetera@gmail.com>
2024-12-18 12:15:27 -0300

    added authors to description; added welcome message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(pkgname) {
+  packageStartupMessage("For the Receptor Noise-Limited Model, v2.1.0 now normalises the abundance of each photoreceptor when argument noise=FALSE.")
+}
