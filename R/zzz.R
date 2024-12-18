8986654029185e2346df5f2f5b5bfd9e3fa09d4e
fgawry <f.etcetera@gmail.com>
2024-12-18 12:55:49 -0300

    a

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(pkgname) {
-  packageStartupMessage("For the Receptor Noise-Limited Model, v2.1.0 normalises the abundance of each photoreceptor when argument noise=FALSE.")
-}
